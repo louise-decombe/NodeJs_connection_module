b73c55b45c17727166a6de93c979919fe1590a44
louise <louise.decombe@laplateforme.io>
2020-11-20 11:54:21 +0100

    register select

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,20 @@
+const mysql = require("mysql");
+
+//connection to the database
+const db = mysql.createConnection({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE
+});
+
+
 exports.register = (req, res) =>{
     console.log(req.body);
+
+const {name, email, password, passwordConfirm} = request.body;
+
+db.query('SELECT email FROM users WHERE email = ?')
+
     res.send("Form submitted");
 }
