94c27da341bcb23b3458a9f5b473ef5d4b38211a
louise <louise.decombe@laplateforme.io>
2020-11-20 10:00:29 +0100

    views + module installation + express connection

---
+++ app.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const path = require("path");
+const mysql = require("mysql");
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './.env'});
+
+const app = express();
+
+const db = mysql.createConnection({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE
+});
+
+const publicDirectory = path.join(__dirname, './public');
+app.use(express.static(publicDirectory));
+app.set('view engine', 'hbs');
+
+
+db.connect( (error) => {
+if(error){
+    console.log(error)
+}else{
+    console.log("mysql connected !")
+}
+})
+
+
+//Define Routes
+app.use('/', require('./routes/pages'));
+
+app.listen(5000,() => {
+    console.log("server started on port 5000")
+})
